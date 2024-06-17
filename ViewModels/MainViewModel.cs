106310fd402a806c9edb4cc34131be6789dc21f7
张龙 <qlzcl@outlook.com>
2024-06-17 12:39:33 +0800

    添加项目文件。

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Controls;
+using AvaloniaApplication1.Views;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Avalonia;
+using Material.Dialog;
+using Material.Dialog.Icons;
+
+namespace AvaloniaApplication1.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    private readonly MainWindow? _window;
+    public bool IsDialogsAvailable { get; }
+    public MainViewModel()
+    {
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime app)
+        {
+            if (app.MainWindow is not MainWindow w)
+                return;
+
+            _window = w;
+            IsDialogsAvailable = true;
+        }
+    }
+
+    [RelayCommand]
+    public void ConfirmDialog()
+    {
+        DialogHelper.CreateAlertDialog(new AlertDialogBuilderParams
+        {
+            ContentHeader = "Confirm action",
+            SupportingText = "Are you sure to DELETE 20 FILES?",
+            StartupLocation = WindowStartupLocation.CenterOwner,
+            NegativeResult = new DialogResult("cancel"),
+            DialogHeaderIcon = DialogIconKind.Help,
+            DialogButtons = new[]
+            {
+                new DialogButton
+                {
+                    Content = "CANCEL",
+                    Result = "cancel"
+                },
+                new DialogButton(){
+                    Content = "DELETE",
+                    Result = "delete"
+                }
+            }
+        }).ShowDialog(_window);
+    }
+
+}
